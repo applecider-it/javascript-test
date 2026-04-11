bebf887adc629fca9a38bd4003a0ee7658f1daf3
owner <owner@localhost>
2026-04-11 09:35:39 +0900

    アセット動作確認

--- src/src/views/HomeView.vue
+++ src/src/views/HomeView.vue
@@ -4,5 +4,9 @@
 <template>
   <div class="app-card">
     トップページ
+    <div class="mt-10">
+      アセット動作確認
+      <img src="../assets/images/AssetBlock.png" class="w-[5rem]" />
+    </div>
   </div>
 </template>
