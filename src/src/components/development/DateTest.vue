e7c427b8bb22403c5ae8026ab6688575d5a1e91f
owner <owner@localhost>
2026-07-17 15:58:31 +0900

    日付関連動作確認

---
+++ src/src/components/development/DateTest.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { format } from 'date-fns';
+
+function getTimezoneOffsetString() {
+  const offset = -new Date().getTimezoneOffset()
+  const sign = offset >= 0 ? '+' : '-'
+  const hours = String(Math.floor(Math.abs(offset) / 60)).padStart(2, '0')
+  const minutes = String(Math.abs(offset) % 60).padStart(2, '0')
+
+  return `${sign}${hours}:${minutes}`
+}
+
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const list = [
+  '2026-07-17T00:10:20',
+  '2026-07-17 00:10:20',
+  '2026-07-17T00:10:20Z',
+  '2026-07-17T00:10:20+09:00',
+];
+</script>
+
+<template>
+  <div>
+    日付関数動作確認
+    <div>TimeZone: {{ timezone }}</div>
+    <div>TimeZone Offset: {{ getTimezoneOffsetString() }}</div>
+    <div class="overflow-x-auto">
+      <table class="min-w-full border border-gray-300 text-sm">
+        <thead class="bg-gray-100">
+          <tr>
+            <th
+              class="border border-gray-300 px-4 py-2 text-left font-semibold"
+            >
+              str
+            </th>
+            <th
+              class="border border-gray-300 px-4 py-2 text-left font-semibold"
+            >
+              toLocaleString()
+            </th>
+            <th
+              class="border border-gray-300 px-4 py-2 text-left font-semibold"
+            >
+              format
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            v-for="str in list"
+            :key="str"
+            class="hover:bg-gray-50 even:bg-gray-50"
+          >
+            <td class="border border-gray-300 px-4 py-2 font-mono">
+              {{ str }}
+            </td>
+
+            <td class="border border-gray-300 px-4 py-2 font-mono">
+              {{ new Date(str).toLocaleString() }}
+            </td>
+
+            <td class="border border-gray-300 px-4 py-2 font-mono">
+              {{ format(new Date(str), 'yyyy年MM月dd日 HH時mm分ss秒') }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
--- src/src/views/DevelopmentView.vue
+++ src/src/views/DevelopmentView.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import DateTest from '@/components/development/DateTest.vue';
+
 </script>
 
 <template>
@@ -7,6 +9,9 @@
     <div class="mt-10">
       アセット動作確認
       <img src="../assets/images/AssetBlock.png" class="w-[5rem]" />
+    </div>
+    <div class="mt-10">
+      <DateTest />
     </div>
   </div>
 </template>
